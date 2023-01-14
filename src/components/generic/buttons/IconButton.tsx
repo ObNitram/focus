1c0afe7fa20521420c822e57b5042fea30825f91
GraberThomas <thomas.graber310@gmail.com>
2023-01-14 19:06:50 +0100

    Change TopBar and Button for stop propagation of event

--- src/components/generic/buttons/IconButton.tsx
+++ src/components/generic/buttons/IconButton.tsx
@@ -4,7 +4,7 @@ import styles from 'styles/components/generic/buttons/iconButton.module.scss'
 
 export interface IconButtonProps {
     title: string;
-    onClick: () => void;
+    onClick: (event:React.MouseEvent) => void;
     icon: React.ReactNode;
     children?: React.ReactNode;
 }
@@ -13,7 +13,7 @@ const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, ref) =
 
 
     return (
-        <button className={styles.icon_button} title={props.title} onClick={props.onClick} ref={ref}>
+        <button className={styles.icon_button} title={props.title} onClick={(e:React.MouseEvent) => props.onClick(e)} ref={ref}>
             {props.icon}
             {props.children}
         </button>
