b6ed0e405db0d539058c464daa32b428d93714fa
GraberThomas <thomas.graber310@gmail.com>
2023-02-04 16:48:37 +0100

    Update cleanJS

--- clean.js
+++ clean.js
@@ -8,6 +8,15 @@ console.log('./modules/pathManage/dist : Removed')
 shell.rm('-rf', './modules/themeTypes/dist')
 console.log('./modules/themeTypes/dist : Removed')
 
+shell.rm('-rf', './dist/')
+console.log('./dist : Removed')
+
+shell.rm('-rf', './dist-electron/')
+console.log('./dist-electron/ : Removed')
+
+shell.rm('-rf', './release/')
+console.log('./release/ : Removed')
+
 shell.rm('-rf', './node_modules/')
 console.log('./node_modules : Removed')
 
