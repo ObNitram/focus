bb732b332403ac4700b2148740f5b63937d0dbd6
Logan-Developer <logan2.humbert@gmail.com>
2023-02-04 15:49:21 +0100

    React: Sidebar: Remove old behavior  where a single note can be shown at opened at the same time

--- src/components/main/sidebar/Sidebar.tsx
+++ src/components/main/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useContext, useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import styles from 'styles/components/main/sidebar.module.scss'
 
