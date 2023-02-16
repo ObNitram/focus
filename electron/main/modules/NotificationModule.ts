6140da8793f1fb7f146b1a6a04cc28624c6a7349
GraberThomas <thomas.graber310@gmail.com>
2023-02-16 18:39:42 +0100

    Add comments

--- electron/main/modules/NotificationModule.ts
+++ electron/main/modules/NotificationModule.ts
@@ -1,11 +1,21 @@
+/**
+ * @file NotificationModule.ts
+ * @description Contains function for send a notification to the main window
+ */
 import * as WindowsManagement from './WindowsManagement'
 
+// The different levels of notification
 export enum NotificationLevelEnum {
     WARNING,
     SUCESS,
     ERROR
 }
 
+/**
+ * @description Send a notification to the main window
+ * @param msg:string The message of the notification
+ * @param level:NotificationLevelEnum The level of the notification
+ */
 export function showNotification(msg: string, level: NotificationLevelEnum) {
     WindowsManagement.getMainWindow()?.webContents.send('notification', { text: msg, level: level })
 }
